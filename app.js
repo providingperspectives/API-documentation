b06bfe729a35ce089964b2b6e941d1ede09622e3
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-02-16 17:43:32 +0100

    second

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express ();
 const swaggerOptions ={
     swaggerDefinition: {
         info:{
-        Title: 'CSE-341 API',
+        title: 'CSE-341 API',
         Version: '1.0.0'
     }
 }, 
@@ -17,28 +17,47 @@ apis: ['app.js'],
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup,(swaggerDocs));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 /**
  * @swagger
  *  /course:
  *     get:
- *       description: Get all Course work
- *       
+ *       description: Get all Course
  *       responses:
  *           200:
  *             description: Success
+ * 
  * 
  */
 
 app.get('/course', (req, res) => {
     res.send(
         [{
-            id: 341,
-            Title: 'Web-Backend 2'
+            id: CSE-341,
+            title: 'Web-Backend 2'
         }]
     )
 });
+
+/**
+ * @swagger
+ *  /course:
+ *     post:
+ *       description: Create a new course 
+ *       responses:
+ *           201:
+ *             description: Created
+ * 
+ */
+
+
+app.post('/course', (req, res) => {res.status(201).send();
+
+})
+
+
+
 
 
 app.listen(8080, () => console.log("listening on 8080")); 
